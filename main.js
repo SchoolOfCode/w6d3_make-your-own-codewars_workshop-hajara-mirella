281b3f68d7d6bd789e41d8f2acaa0e13c9116e1c
Mirella <mirellacodes@gmail.com>
2021-12-22 15:33:55 +0000

    Completed two tests

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 /* 
 👉 Write your kata here!
+
+You will be given a string (e.g "hello" and "bye").
+Find the numbers of the characters in the string
+Square the numbers
+Return the value.
+
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+function squareIt(string) {}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,27 @@
 //👉 Write your tests below here:
+// w
+// We want to check the length of the string (string.length)
+//
+
+describe(`Check if string is the type of string`, function () {
+  test(`Check if string is the type of string`, function () {
+    // arrange
+    const actual = expect.any(String);
+    // act
+    const expected = expect.any(String);
+    // Assert
+    expect(actual).toStrictEqual(expected);
+  });
+});
+
+describe(`Check if the string.length matches the number of that length`, function () {
+  test(``, function () {
+    // arrange
+    const string = "hello";
+    const actual = string.length;
+    // act
+    const expected = string.length;
+    // assert
+    expect(actual).toBe(expected);
+  });
+});

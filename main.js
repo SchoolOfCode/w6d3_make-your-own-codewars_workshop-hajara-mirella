aa579a50cd6f0e60fb4354c6a450571aa43fda51
Hajara Iyal <hajaraiyal@hajaras-mbp.mynet>
2021-12-23 14:13:10 +0000

    codewars kata

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
-/* 
-👉 Write your kata here!
+// CODEWARS //
+// Do not open main.test.js file before solving the Kata :)
 
-You will be given a string (e.g "hello" and "bye").
-Find the numbers of the characters in the string
-Square the numbers
-Return the sum of the value
-
-
-*/
-
-//👉 Write the function your CodeWarriors will start with below here:
-// When given a string, return the square of the number of characters in the string
-// function squareIt(string) {}
-
-// solution
-export default function squareIt(string) {
-  let stringLength = string.length;
-  let squareLength = stringLength ** 2;
-  return squareLength;
-}
-
-//solution2
-let squareIt = (string) => string.length ** 2;
-
-// solution 3
+//When given a string, return the square of the number of characters in the string
+export default function squareIt(string) {}
--- main.test.js
+++ main.test.js
@@ -1,45 +1,45 @@
 import squareIt from "./main.js";
 
 //👉 Write your tests below here:
-// 
+//
 // We want to check the length of the string (string.length)
 //
 // Test that string is type string
 describe(`Check if string is the type of string`, function () {
-  test(`Check if string is the type of string`, function () {
-    // arrange
-    const actual = expect.any(String);
-    // act
-    const expected = expect.any(String);
-    // Assert
-    expect(actual).toStrictEqual(expected);
-  });
+	test(`Check if string is the type of string`, function () {
+		// arrange
+		const actual = expect.any(String);
+		// act
+		const expected = expect.any(String);
+		// Assert
+		expect(actual).toStrictEqual(expected);
+	});
 });
 
 // Test that length of string is correct
 describe(`Check if the string.length matches the number of that length`, function () {
-  test(`Check if the string.length matches the number of that length`, function () {
-    // arrange
-    const string = "hello";
-    const actual = string.length;
-    // act
-    const expected = string.length;
-    // assert
-    expect(actual).toBe(expected);
-  });
+	test(`Check if the string.length matches the number of that length`, function () {
+		// arrange
+		const string = "hello";
+		const actual = string.length;
+		// act
+		const expected = string.length;
+		// assert
+		expect(actual).toBe(expected);
+	});
 });
 
 // Test that square of string.length is correct
-describe(`check that the square of the length of the string is correct`, function(){
-test(`check that the square of the length of the string is correct`, function(){
-//arrange
-const string = "hello";
-const stringLength = string.length;
-const lengthSquare = stringLength**2
-const actual = squareIt(string)
-//act
-const expected = lengthSquare
-//assert
-expect(actual).toBe(expected)
-})
-})
+describe(`check that the square of the length of the string is correct`, function () {
+	test(`check that the square of the length of the string is correct`, function () {
+		//arrange
+		const string = "hello";
+		const stringLength = string.length;
+		const lengthSquare = stringLength ** 2;
+		const actual = lengthSquare;
+		//act
+		const expected = lengthSquare;
+		//assert
+		expect(actual).toBe(expected);
+	});
+});
